6036506ebe507cc5ede76711252234bd555ec6d3
Diego Viana <dtviana90@gmail.com>
2024-07-19 23:49:31 -0300

    feat: Implementa UpdateCompany

--- src/components/UpdateCompany.tsx
+++ src/components/UpdateCompany.tsx
@@ -1,27 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Layout from './Layout';
 import { formatCEPNumber, formatCNPJandCPF, formatPhoneNumber } from '../mask/mask';
-import axios from 'axios';
 
-const Companies: React.FC = () => {
-    const [companyData, setCompanyData] = useState({
-        name: '',
-        tradeName: '',
-        cnpjCpf: '',
-        cep: '',
-        street: '',
-        propertyNumber: '',
-        district: '',
-        city: '',
-        state: '',
-        country: 'Brasil',
-        addressComplement: '',
-        phoneNumber: '',
-        email: '',
-        observations: ''
-    });
-    const [loading, setLoading] = useState(false);
-    const [companyExists, setCompanyExists] = useState(false);
+interface UpdateCompanyProps {
+    initialData: {
+        cnpjCpf: string;
+        name: string;
+        tradeName: string;
+        phoneNumber: string;
+        email: string;
+        cep: string;
+        street: string;
+        propertyNumber: string;
+        addressComplement: string;
+        district: string;
+        city: string;
+        state: string;
+        country: string;
+        observations: string;
+    };
+}
+
+const UpdateCompany: React.FC<UpdateCompanyProps> = ({ initialData }) => {
+    const [companyData, setCompanyData] = useState(initialData);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = event.target;
@@ -45,37 +46,15 @@ const Companies: React.FC = () => {
         });
     };
 
-    useEffect(() => {
-        if (companyData.cnpjCpf.replace(/\D/g, '').length === 14 || companyData.cnpjCpf.replace(/\D/g, '').length === 11) {
-            checkCompanyExists(companyData.cnpjCpf);
-        }
-    }, [companyData.cnpjCpf]);
-
-    const checkCompanyExists = async (taxNumber: string) => {
-        setLoading(true);
-        try {
-            const response = await axios.get(`/api/company/GetCompanyByTaxNumber`, { params: { taxNumber: taxNumber.replace(/\D/g, '') } });
-            if (response.data) {
-                setCompanyExists(true);
-                alert('Cliente já está cadastrado.');
-            }
-        } catch (error) {
-            console.log('Cliente não encontrado.');
-        } finally {
-            setLoading(false);
-        }
-    };
-
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log("Dados da Empresa:", companyData);
+        console.log("Atualizar Dados da Empresa:", companyData);
     };
 
     return (
         <Layout>
             <main className="companies-content">
-                {loading && <div className="loading-overlay">Carregando...</div>}
-                <h1>Cadastro de Clientes</h1>
+                <h1>Atualização de Clientes</h1>
                 <form onSubmit={handleSubmit}>
                     <input type="text" name="cnpjCpf" placeholder="CNPJ XX.XXX.XXX/XXXX-XX OU CPF XXX.XXX.XXX-XX" value={companyData.cnpjCpf} onChange={handleChange} className="companies-input" />
                     <input type="text" name="name" placeholder="Nome do Cliente" maxLength={60} value={companyData.name} onChange={handleChange} className="companies-input" />
@@ -91,11 +70,11 @@ const Companies: React.FC = () => {
                     <input type="text" name="state" placeholder="Estado" maxLength={30} value={companyData.state} onChange={handleChange} className="companies-input" />
                     <input type="text" name="country" placeholder="País" maxLength={30} value={companyData.country} onChange={handleChange} className="companies-input" />
                     <textarea name="observations" placeholder="Observações" maxLength={100} value={companyData.observations} onChange={handleChange} className="companies-input"></textarea>
-                    <button type="submit" className="submit-button">Registrar Empresa</button>
+                    <button type="submit" className="submit-button">Atualizar Empresa</button>
                 </form>
             </main>
         </Layout>
     );
 };
 
-export default Companies;
+export default UpdateCompany;
